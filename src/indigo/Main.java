0574881a341e9ba53ce871088a4ce1249a0942ba
thaiphan <thaiphan2712@gmail.com>
2025-04-20 14:14:01 -0500

    update

--- src/indigo/Main.java
+++ src/indigo/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Expense Tracker?");
+        System.out.println("Welcome to Expense Tracker!");
 
 
 //        Scanner scanner = new Scanner(System.in);
